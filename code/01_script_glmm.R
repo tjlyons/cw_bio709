fd96d0fd11aa497860bc42ec9ad9e2f2820d5609
tjlyons <tjlyons@uncg.edu>
2026-02-11 17:28:31 -0500

    GLMM Complete

--- code/01_script_glmm.R
+++ code/01_script_glmm.R
@@ -3,6 +3,69 @@
 
 # in-class ----------------------------------------------------------------
 
+pacman::p_load(tidyverse,
+               lme4,
+               glmmTMB,
+               janitor)
+
+# Group Structure
+
+data(Owls)
+df_owl_raw <- as_tibble(Owls)
+view(df_owl_raw)
+
+df_owl <- df_owl_raw %>% 
+  janitor::clean_names() %>% # standardize col names
+  mutate(across(.cols = where(is.factor), # select factor-type cols
+                .fns = str_to_lower)) # convert factors to lowercase
+view(df_owl)
+
+df_owl %>%  # Generating box plot forg negotions per chick
+  ggplot(aes(x = food_treatment,
+             y = neg_per_chick)) +
+  geom_boxplot(outliers = FALSE) + 
+  geom_jitter(alpha = 0.25) + 
+  theme_bw()
+
+v_g9 <- unique(df_owl$nest)[1:9] # first 9 nest IDs
+v_g9
+df_owl %>% 
+  filter(nest %in% v_g9) %>% # Select only data from first 9 nests
+  ggplot(aes(x = food_treatment,
+             y = neg_per_chick))+
+  geom_jitter(alpha = 0.25,
+              width = 0.1) + 
+  facet_wrap(facets =~ nest, # Separate panels per nest
+             ncol = 3,
+             nrow = 3) +
+  theme_bw()
+
+m_glm <- MASS::glm.nb(sibling_negotiation ~ food_treatment +
+                        nest +
+                        offset(log(brood_size)),
+                      data = df_owl)
+# Random Intercept
+
+m_ri <- glmmTMB(
+  sibling_negotiation ~ # response
+    food_treatment + # fixed effect
+    (1 | nest) + # random intercept for each nest
+    offset(log(brood_size)), # offset to model rate per chick
+  data = df_owl,
+  family = nbinom2()) # negative binomial dist
+summary(m_ri)
+
+head(coef(m_ri)$cond$nest) # group specific intercepts
+
+m_ris <- glmmTMB(sibling_negotiation ~ food_treatment +
+                   (1 + food_treatment | nest) + # random int/slope per nest
+                   offset(log(brood_size)), # rate per chick
+                 data = df_owl,
+                 family= nbinom2())
+summary(m_ris)
+
+head(coef(m_ris)$cond$nest)
+
 
 # lab ---------------------------------------------------------------------
 
@@ -27,6 +90,8 @@
 #   ID    : factor identifying each subject; Each subject appears once in each group
 # ------------------------------------------------------------
 
+view(sleep)
+
 # Q1 – Visualization:
 # Compare sleep increase ("extra") between the two drug groups.
 #
@@ -35,6 +100,16 @@
 # - Highlight paired structure (same subject in both groups)
 # - Use color to identify subjects
 # - Connect observations from the same subject using lines
+
+sleep %>% 
+  ggplot(aes(x = group,
+             y = extra,
+             color = ID,
+             group = ID)) +
+         geom_point() +
+           geom_line() +
+  labs( x = "Treatment Group",
+        y = "Extra sleep (hours)")
 
 # Q2 - Model development:
 #
@@ -46,6 +121,13 @@
 #   - Drug (group) represents the treatment of interest
 #   - Subject ID represents repeated measurements on the same
 #     individuals
+
+lm_sleep <- lm(extra ~ group, data = sleep)
+summary(sleep) # bad, since repeat measurements (paired)
+
+lmm_sleep <- lmer(extra ~ group + (1|ID), data = sleep) # We need random intercept for ID, right?
+summary(lmm_sleep) # Drug 2 increases mean sleep by 1.6 hrs more than drug 1 on average
+# after random effects
 
 # ============================================================
 # EXERCISE: GLMM Exercise - `grouseticks` Data Set
@@ -88,6 +170,23 @@ data("grouseticks")
 # - Plot mean ticks vs. height
 # - Color points by sampling year
 
+view(grouseticks)
+grouseticks <- grouseticks %>%
+  clean_names(case = "snake") # I think this makes it lowercase?
+
+brood_ticks_mu <- grouseticks %>% 
+  group_by(brood, height, year) %>% 
+  summarize(mu_ticks = mean(ticks))
+view(brood_ticks_mu)
+
+brood_ticks_mu %>% 
+  ggplot(aes(x = height,
+              y = mu_ticks,
+             color = factor(year))) +
+  geom_point() +
+  labs(x = "Height (m)",
+       y = "Mean Ticks per Brood")
+
 # Q2 – Model development:
 #
 # Goal:
@@ -98,3 +197,9 @@ data("grouseticks")
 #   - HEIGHT represents the variable of interest
 #   - BROOD represents a grouping factor of repeated measurements
 #   - YEAR represents another grouping factor of repeated measurements
+
+glmm_grouseticks <- glmer(ticks ~ height + (1|brood) + (1|year),
+                          data = grouseticks,
+                          family = poisson) # Unsure if poisson is the way to go
+summary(glmm_grouseticks) # Height has a significant effect on mean ticks per brood
+# when accounting for year and brood as random effects
